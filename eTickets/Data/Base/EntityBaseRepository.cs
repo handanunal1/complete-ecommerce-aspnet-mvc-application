e8f9490cf3277ffec8ae621f46878490aa245da8
handan unal <handan.unal@yahoo.com>
2022-05-22 23:21:09 +0300

    generic class for crud op.

---
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -0,0 +1,43 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Base
+{
+    public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
+    {
+        private readonly AppDbContext _appDbContext;
+        public EntityBaseRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public Task AddAsync(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+{
+            var result = await _appDbContext.Set<T>().ToListAsync();
+            return result;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+{
+            var result = await _appDbContext.Set<T>().FirstOrDefaultAsync(x => x.Id == id);
+            return result;
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
